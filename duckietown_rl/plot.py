e3f2af21c804f3eac9cbe4bf0a518e2c6916501d
pkhad1 <98765802+pkhad1@users.noreply.github.com>
2022-04-28 20:44:18 -0400

    Add files via upload

---
+++ duckietown_rl/plot.py
@@ -0,0 +1,15 @@
+
+
+
+import baseline
+from baselines.common import plot_util as plot
+import matplotlib.pyplot as plt
+import numpy as np
+
+results = plot.load_results('~/results/DDPG_123')
+print(results)
+r = results[0]
+plt.plot(np.cumsum(r.monitor.l), plot.smooth(r.monitor.r, radius=10))  # radius to smoother
+plt.xlabel('Iterations')
+plt.ylabel('Reward')
+plt.show()
